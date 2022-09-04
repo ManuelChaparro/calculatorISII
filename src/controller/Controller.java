5c64b4db501982286529a737760f3de05992fe04
Santiago Rojas <manuel.chaparro01@uptc.edu.co>
2022-09-04 01:52:57 -0500

    Funcionalidades añadidas: Suma, resta, multiplicacion y division

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,17 @@
 package controller;
 
+import models.Calculator;
+
 public class Controller {
 
+    private static Calculator calculator;
+    private static final int NUM1 = 10;
+    private static final int NUM2 = 5;
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        calculator = new Calculator();
+        System.out.println(calculator.sum(NUM1, NUM2));
+        System.out.println(calculator.subtraction(NUM1, NUM2));
+        System.out.println(calculator.multiplication(NUM1, NUM2));
+        System.out.println(calculator.split(NUM1, NUM2));
     }
 }
